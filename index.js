87c4f36f94513b53bcff58ff088ee9b21cdbb7ad
joselmm <josealfredosoraca@gmail.com>
2026-03-16 08:47:44 -0500

    se aplica cors

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import { db } from './modules/db.js';
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
-console.log()
+app.use(cors());
 
 app.set('trust proxy', true);
 
